a139c60dd6515e032aaabd441d7fc844b334a0a1
Thomas Liu <tommyliu9@gmail.com>
2019-05-12 18:29:42 -0400

    refactored packages

---
+++ src/components/NavBar.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Alert } from "shards-react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
+import {
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  FormInput,
+  Collapse
+} from "shards-react";
+//npm run
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+
+    this.state = {
+      dropdownOpen: false,
+      collapseOpen: false
+    };
+  }
+
+  toggleDropdown() {
+    this.setState({
+      ...this.state,
+      ...{
+        dropdownOpen: !this.state.dropdownOpen
+      }
+    });
+  }
+
+  toggleNavbar() {
+    this.setState({
+      ...this.state,
+      ...{
+        collapseOpen: !this.state.collapseOpen
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Navbar type="dark" theme="primary" expand="md">
+        <NavbarBrand href="#">Thomas Liu</NavbarBrand>
+        <NavbarToggler onClick={this.toggleNavbar} />
+
+        <Collapse open={this.state.collapseOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink active href="#">
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="#">
+                Resume
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="#">
+                Projects
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
+}
